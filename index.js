455fc9bc8fa3f53a6c899e6c6c5d58e2d29827a4
liuxin <475212506@qq.com>
2018-01-15 17:12:26 +0800

    init commit

---
+++ index.js
@@ -0,0 +1,17 @@
+"use strict"
+
+module.exports = {
+  "extends": "stylelint-config-standard",
+  "rules": {
+    "indentation": 4,
+    "max-empty-lines": 4,
+    "string-quotes": "single",
+    "function-url-quotes": "single",
+    "color-hex-length": "long",
+    "declaration-colon-newline-after": null,
+    "font-family-name-quotes": null,
+    "number-leading-zero": "never",
+    "value-list-comma-newline-after": null,
+    "max-nesting-depth": 2
+  }
+}
